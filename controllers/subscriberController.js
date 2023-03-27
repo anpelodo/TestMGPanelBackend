070df0ab3f5ebe9c9257cddead71a87da97317e2
Juan Felipe Giraldo <juanfegiraldo97@gmail.com>
2023-03-26 20:06:58 -0500

    Subscriptor controller, defined list method with pagination

--- controllers/subscriberController.js
+++ controllers/subscriberController.js
@@ -1,5 +1,41 @@
+const Subscriber = require("../models/subscriber");
+
+const sendErrorMsg = (error, res) => {
+  return res.status(500).json({
+    status: 500,
+    payload: {
+      message: "Something happens",
+      error,
+    },
+  });
+};
+
 module.exports = {
-  async list(req, res) {},
+  async list(req, res) {
+    const pageSize = Math.max(req.body.pageSize || req.query.pageSize || 10, 1);
+    let page = Math.max(req.body.page || req.query.page || 1, 1);
+
+    try {
+      const totalSubscribers = await Subscriber.find().estimatedDocumentCount();
+      const numberOfPages = Math.floor(totalSubscribers / pageSize) + 1;
+
+      const subscribers = await Subscriber.find()
+        .skip((page - 1) * pageSize)
+        .limit(pageSize);
+
+      return res.status(200).json({
+        status: 200,
+        payload: {
+          numberOfPages,
+          page,
+          totalSubscribers,
+          subscribers,
+        },
+      });
+    } catch (error) {
+      sendErrorMsg(error, res);
+    }
+  },
 
   async add(req, res) {},
 
